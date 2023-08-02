e589fea6d461f10636ce5f1d0178defb99c4735b
DaniilStepanov2000 <mrdanilusha@gmail.com>
2023-08-02 16:18:31 +0300

    fixed docker

--- schedule_reports_email/settings.py
+++ schedule_reports_email/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'schedule_reports_email.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
